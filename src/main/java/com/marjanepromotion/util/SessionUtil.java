1b0ce910a198ab87456e80f35655af0d02428938
TahaLechgar <tahamr08@gmail.com>
2022-10-25 11:08:34 +0100

    add AbstractHibernateDao to implement necessary CRUD operations

---
+++ src/main/java/com/marjanepromotion/util/SessionUtil.java
@@ -0,0 +1,36 @@
+package com.marjanepromotion.util;
+
+import jakarta.el.ExpressionFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class SessionUtil {
+
+    private static SessionFactory sessionFactory = null;
+
+    protected static void setUp()  {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    public static Session getCurrentSession() {
+        if(sessionFactory == null){
+            setUp();
+        }
+        return sessionFactory.getCurrentSession();
+    }
+}
